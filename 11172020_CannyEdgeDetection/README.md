0791594fcdccfe3698d7237d1dd5094335453af9
akiencore <akiencore@gmail.com>
2020-11-18 17:58:36 -0500

    CannyEdgeDetection:

---
+++ 11172020_CannyEdgeDetection/README.md
@@ -0,0 +1,36 @@
+# Nov 17, 2020 - Apply Canny Edge Detection
+
+## Purpose: 
+Apply OpenCV to Microsoft Visual Studio to: 
+* Load an image and transform it into a grayscale image
+* 
+    
+</br>
+
+## Environment settings:
+
+* Programming Languages: 
+    * C++
+* Microsoft Visual Studio 2017
+* OpenCV 3.2 (x64)
+
+</br>
+
+## How to build: 
+1. Open \vc15-3.2\OpenCVExample.sln in Visual Studio
+2. In Menu, select "Build"->"Build Solution"
+
+## How to run:
+### In visual studio
+1. Bulid Solution
+2. Open \vc15-3.2\OpenCVExample.sln in Visual Studio
+3. In Menu, select "Debug"->"Start Without Debugging"
+### In cmd.exe
+1. get into the directory with the image you want to load in the cmd.exe
+2. copy the path of "OpenCVExample.exe" in vc15-3.2\x64\Release\
+3. input "[path] [image_name]" in cmd.exe
+
+## How to modify source code:
+1. Modify \vc15-3.2\OpenCVExample\OpenCVExample.cpp
+
+</br>
---
+++ 11172020_CannyEdgeDetection/vc15-3.2/OpenCVExample/OpenCVExample.cpp
@@ -0,0 +1,67 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+#define NORMAL_HT
+
+int edgeThreshold = 100;
+Mat image, gray, edge, cedge; //cedge is "colored edge", edge is "grayscale edge"
+//Mat here represents image, an image is a matrix of pixel color intensity 
+
+// define a trackbar callback
+static void onTrackbar(int, void*)
+{
+	//blur grayscale image to edge image with a 5*5 matrix
+	blur(gray, edge, Size(5, 5));
+
+	//run the edge detector on grayscale
+	//Canny Edge Detector: upper threshold is 2*threshold, lower threshold is threshold
+	Canny(edge, edge, edgeThreshold, edgeThreshold * 2, 3);
+
+	//set the scalar of colored edge matrix into 0
+	cedge = Scalar::all(0);
+	//used filtered edge as mask to copy edge data into cedge, and get a colored edge image
+	image.copyTo(cedge, edge);
+
+	imshow("Edge map", cedge);
+	imshow("Grayscale Edge map", edge);
+}
+
+int main(int argc, char** argv)
+{	
+	//if got the input of 2 strings, the filename is the 2nd input string
+	//otherwise, the default file name is "image01.jpg"
+	string filename = argc == 2 ? argv[1] : (char*)"image01.jpg";
+
+	//load the image with the filename into image
+	//the second parameter is "is a colored image?"
+	image = imread(filename, 1);
+
+	//if that image doesn't exist, return error
+	if (image.empty())
+	{
+		printf("Cannot read image file: %s\n", filename.c_str());
+		return -1;
+	}
+
+	//create a colored edge matrix with the size and type of the image
+	cedge.create(image.size(), image.type());
+	//convert image to grayscale image
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+
+	// create a window of the edge map
+	namedWindow("Edge map", 1);
+
+	// create a trackbar
+	createTrackbar("Canny threshold", "Edge map", &edgeThreshold, 200, onTrackbar);
+
+	onTrackbar(0, 0);
+
+	waitKey(0);
+
+	return 0;
+}
